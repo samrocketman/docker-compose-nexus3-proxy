d7717aa00b4c486a56241bf81b9467bc1386aeba
Sam Gleske <sam.mxracer@gmail.com>
2018-04-22 18:21:40 -0700

    run_nexusconfiguration.sh script

---
+++ scripts/run_nexusConfiguration.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Created by Sam Gleske
+#License: ASLv2 https://github.com/samrocketman/docker-compose-local-nexus3-proxy
+#Sun Apr 22 15:53:51 PDT 2018
+#Ubuntu 16.04.4 LTS
+#Linux 4.13.0-38-generic x86_64
+#GNU bash, version 4.3.48(1)-release (x86_64-pc-linux-gnu)
+#curl 7.47.0 (x86_64-pc-linux-gnu) libcurl/7.47.0 GnuTLS/3.5.5 zlib/1.2.8 libidn/1.32 librtmp/2.3
+
+# DESCRIPTION:
+#     Upload script to Nexus 3 scripting endpoint.
+
+NEXUS_ENDPOINT="${NEXUS_ENDPOINT:-http://localhost:8081}"
+# remove trailing slashes
+NEXUS_ENDPOINT="${NEXUS_ENDPOINT%/}"
+NEXUS_USER="${NEXUS_USER:-admin}"
+NEXUS_PASSWORD="${NEXUS_PASSWORD:-admin123}"
+
+curl -u "${NEXUS_USER}:${NEXUS_PASSWORD}" -X POST -d @"$1" \
+  --header 'Content-Type: text/plain' \
+  "${NEXUS_ENDPOINT}"/service/rest/v1/script/nexusConfiguration/run
