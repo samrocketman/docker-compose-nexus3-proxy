3b4ed7263a09b637473c3e4a33034bf39a8f013e
Sam Gleske <sam.mxracer@gmail.com>
2018-05-06 12:34:47 -0700

    Allow argument on wait-for-ready script

--- scripts/wait-for-ready.sh
+++ scripts/wait-for-ready.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Created by Sam Gleske
+nexus_url="${1:-http://localhost:8081}"
 until curl -fsSIu admin:admin123 -m 15 -o /dev/null \
-      http://localhost:8081/service/metrics/ping; do
+      "${nexus_url%/}"/service/metrics/ping; do
   sleep 5
 done
